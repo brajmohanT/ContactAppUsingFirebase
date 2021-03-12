2ffbd999e6084878a84f7251991318c59ba86a0a
Brajmohan Tikarya <62324112+brajmohanT@users.noreply.github.com>
2021-03-12 20:10:53 +0530

    Add files via upload

---
+++ Eleven-Realtimedatabase/src/utils/config.js
@@ -0,0 +1,20 @@
+//TODO: add firebase configuration and export it
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDHUZGdlyfMfD7NpEZWgo8wohNAzGcQ2Do",
+    authDomain: "mygitapp-b373b.firebaseapp.com",
+    databaseURL: "https://mygitapp-b373b-default-rtdb.firebaseio.com",
+    projectId: "mygitapp-b373b",
+    storageBucket: "mygitapp-b373b.appspot.com",
+    messagingSenderId: "39354791498",
+    appId: "1:39354791498:web:f3c6c99c6a10dd509018b3",
+    measurementId: "G-2HN3R8R209"
+};
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
